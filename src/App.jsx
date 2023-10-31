24206ec0db72247da1d2f5991cc4d90d1f8717a0
Alec Venida <alecvenida@gmail.com>
2023-10-31 15:27:28 -0700

    first tone and oscillator

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 
 import './App.scss'
 
+// create our audio context as one instance; our react state will match our web audio current state
+const actx = new AudioContext();
+
+//save our state, and update it in our audio context and react state
+const out = actx.destination;
+const osc1 = actx.createOscillator();
+
+const gain1 = actx.createGain();
+
+osc1.connect(gain1);
+gain1.connect(out);
+
+osc1.start();
 function App() {
 
 
   return (
     <>
-
+    <div className='App'>
+      <h1>Alec-Synth</h1>
+    </div>
     </>
   )
 }
