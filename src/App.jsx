3819ff6265f8be7e9c03aa9eb540b226c7bf9ac0
Alec Venida <alecvenida@gmail.com>
2023-10-31 15:29:53 -0700

    annotate each piece

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ import './App.scss'
 const actx = new AudioContext();
 
 //save our state, and update it in our audio context and react state
+
+//this is our out, think of it like an amp
 const out = actx.destination;
+
+//this is our first oscillator. think of it like a string; synthesis vibration
 const osc1 = actx.createOscillator();
-
+// all oscillators need a gain in order to have sound.
 const gain1 = actx.createGain();
-
+//connect our first oscillator to our first gain
 osc1.connect(gain1);
+//connect that gain to the out
 gain1.connect(out);
 
+//start it up
 osc1.start();
 function App() {
 
