56b5e16eb7eff6aa86da501ed0389223b6bd17f2
Alec Venida <alecvenida@gmail.com>
2023-11-02 18:40:59 -0700

    added Filter component; imported filter into App; need to pass props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import "./App.scss";
-import Osc1 from "./components/Osc1";
 import React from "react";
 import { useState } from "react";
+import Osc1 from "./components/Osc1";
+import Filter from "./components/Filter";
+import "./App.scss";
 
 // create our audio context as one instance; our react state will match our web audio current state
 const actx = new AudioContext();
@@ -11,15 +12,23 @@ const out = actx.destination;
 
 //this is our first oscillator. think of it like a string; synthesis vibration
 const osc1 = actx.createOscillator();
+
 // all oscillators need a gain in order to have sound.
 const gain1 = actx.createGain();
+
+//set up the biquad filter
+const filter = actx.createBiquadFilter();
+
+
 //connect our first oscillator to our first gain
 osc1.connect(gain1);
-//connect that gain to the out
-gain1.connect(out);
+//connect the gain to the filter
+gain1.connect(filter);
+//connect the filter to the out
+filter.connect(out);
 
-//start it up
-// osc1.start();
+
+
 
 function App() {
   const [osc1Settings, setOsc1Settings] = useState({
@@ -27,6 +36,14 @@ function App() {
     detune: osc1.detune.value,
     type: osc1.type
   });
+
+  const [filterSettings, setFilterSettings] = useState({
+    frequency: filter.frequency.value,
+    detune: filter.detune.value,
+    Q: filter.Q.value,
+    gain: filter.gain.value,
+    type: filter.type
+  })
 
   const changeOsc1 = (e) => {
     const { value, id } = e.target;
@@ -40,6 +57,12 @@ function App() {
     setOsc1Settings({...osc1Settings, type: id})
   };
 
+  const changeFilter = e => {
+    const {value, id} = e.target;
+    setFilterSettings({...filterSettings, [id]: value});
+    filter[id].value = value;
+  }
+
   //notice with these buttons, you still can't start more than once; this is why Tone.js is important TODO
   return (
     <>
@@ -51,6 +74,10 @@ function App() {
           change={changeOsc1}
           settings={osc1Settings}
           changeType={osc1Type}
+        />
+        <Filter
+        change={changeFilter}
+        settings={filterSettings}
         />
       </div>
     </>
