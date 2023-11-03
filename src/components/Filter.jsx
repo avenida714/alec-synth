0b4f19ca005a8205c8c1ba01548229e21123a264
Alec Venida <alecvenida@gmail.com>
2023-11-02 18:46:05 -0700

    freq, detune, q and gain in filter; styles applied

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const Filter = () => {
+const Filter = ({change, settings}) => {
   return (
-    <div>
+    <div className='control'>
       <h2>Filter</h2>
+      <div className="params">
+        <h3>Frequency</h3>
+        <input type="range" onChange={change} id="frequency" max="10000" />
+      </div>
+      <div className="params">
+        <h3>Detune</h3>
+        <input type="range" onChange={change} id="detune"  />
+      </div>
+      <div className="params">
+        <h3>Q</h3>
+        <input type="range" onChange={change} id="Q" max="10" />
+      </div>
+      <div className="params">
+        <h3>Gain</h3>
+        <input type="range" onChange={change} id="gain" max="10" />
+      </div>
     </div>
   )
 }
