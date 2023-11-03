060c462206e74b77bee4c71720121d07ed59fe22
Alec Venida <alecvenida@gmail.com>
2023-11-03 10:15:46 -0700

    new error, can't call start() more than once

--- src/App.jsx
+++ src/App.jsx
@@ -17,3 +17,44 @@ function App() {
 }
 
 export default App;
+
+/*
+
+Uncaught DOMException: AudioScheduledSourceNode.start: Can't call start() more than once
+    reducer Store.jsx:34
+    React 3
+    Store Store.jsx:60
+    React 17
+    onClick Osc1.jsx:23
+    React 23
+    <anonymous> main.jsx:7
+Store.jsx:34:6
+    reducer Store.jsx:34
+    React 3
+    Store Store.jsx:60
+    React 17
+    onClick Osc1.jsx:23
+    React 19
+    forEach self-hosted:4409
+    React 4
+    <anonymous> main.jsx:7
+The above error occurred in the <Store> component:
+
+Store@http://127.0.0.1:5173/src/context/Store.jsx:83:27
+
+Consider adding an error boundary to your tree to customize error handling behavior.
+Visit https://reactjs.org/link/error-boundaries to learn more about error boundaries. 127.0.0.1:5173:12966:25
+Uncaught DOMException: AudioScheduledSourceNode.start: Can't call start() more than once
+    reducer Store.jsx:34
+    React 3
+    Store Store.jsx:60
+    React 14
+    onClick Osc1.jsx:23
+    React 23
+    <anonymous> main.jsx:7
+Store.jsx:34:6
+Source map error: Error: request failed with status 404
+Resource URL: null
+Source Map URL: react_devtools_backend_compact.js.map
+
+*/
