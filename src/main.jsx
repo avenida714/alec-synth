62901a0aba74fa7c6737eb0c970dcfdaac146ad8
Alec Venida <alecvenida@gmail.com>
2023-11-03 09:10:44 -0700

    Store.js component; refactor state from App into Store; wrap store around App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Store from './context/Store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Store>
     <App />
+    </Store>
   </React.StrictMode>,
 )
