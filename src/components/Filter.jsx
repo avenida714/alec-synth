39817e472fb0ba29efc4a17aeb06b0eb9c6bb759
Alec Venida <alecvenida@gmail.com>
2023-11-02 18:52:42 -0700

    filter working as low pass

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const Filter = ({change, settings}) => {
+const Filter = ({ change, settings }) => {
+  const { frequency, detune, Q, gain, type } = settings;
+
   return (
-    <div className='control'>
+    <div className="control">
       <h2>Filter</h2>
       <div className="params">
         <h3>Frequency</h3>
-        <input type="range" onChange={change} id="frequency" max="10000" />
+        <input
+          value={frequency}
+          type="range"
+          onChange={change}
+          id="frequency"
+          max="10000"
+        />
       </div>
       <div className="params">
         <h3>Detune</h3>
-        <input type="range" onChange={change} id="detune"  />
+        <input value={detune} type="range" onChange={change} id="detune" />
       </div>
       <div className="params">
         <h3>Q</h3>
-        <input type="range" onChange={change} id="Q" max="10" />
+        <input value={Q} type="range" onChange={change} id="Q" max="10" />
       </div>
       <div className="params">
         <h3>Gain</h3>
-        <input type="range" onChange={change} id="gain" max="10" />
+        <input value={gain} type="range" onChange={change} id="gain" max="10" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
